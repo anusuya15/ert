84414084ffe084a5ac5cefefbc8d05266ab75768
anusuya15 <anusuyaindhu5@gmail.com>
2016-08-08 03:11:09 -0700

    sdfgg
    sdfg

---
+++ num1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Test5 {
+public static void main(String[] args) {
+	Scanner in =new Scanner(System.in);
+	System.out.println("enter the length of array");
+	int l=in.nextInt();
+	int arr[]=new int[l];
+	System.out.println("enter the elements");
+	for(int l=0;l<n;l++)
+	{
+	arr[l]=in.nextInt();	
+	}
+	for(int j=0;j<arr.length;j++)
+	{
+		if(arr[j]==j)
+		{
+			System.out.println(j);
+		}
+		
+		}
+}
+}
+
+		
+
+	
+
+	
+	 
+	
+	    
